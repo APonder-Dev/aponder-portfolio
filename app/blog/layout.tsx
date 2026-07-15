a4a9738e35464369127e802df0850342f3d8e21f
Anthony Ponder <Anthony@aponder.dev>
2026-07-14 19:26:24 -0500

    Add footer to blog layout

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,6 +1,12 @@
 import 'highlight.js/styles/github-dark.css'
+import Footer from '@/components/Footer'
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  )
 }
 
